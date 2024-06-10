c4fe4016c884a146fbd179a201b654cafb34b987
Rizen <123223112+Richard0070@users.noreply.github.com>
2024-06-10 18:18:16 +0530

    🎐
    🎐

--- views/event_apps_views.py
+++ views/event_apps_views.py
@@ -1,64 +1,39 @@
 import discord
-from discord.utils import get
 import json
-import config 
 import os
+import config 
+from discord.utils import get
 
-class EventAppModal(discord.ui.Modal,
-                    title='Fill this to apply for events team'):
-
+class EventAppModal(discord.ui.Modal):
     def __init__(self, bot):
-        super().__init__(timeout=3600)
-        self.bot = bot
-        self.q1 = discord.ui.TextInput(
-            label='Tell us about you!',
-            placeholder=
-            'Who are you, what is your age (necessary) and what are you interested in, your hobbies, etc.',
-            style=discord.TextStyle.long,
-            max_length=1018,
-            required=True)
-        self.q2 = discord.ui.TextInput(label='What is your timezone?',
-                                       placeholder='Example: GMT +5:30',
-                                       required=True,
-                                       style=discord.TextStyle.short,
-                                       max_length=10)
-        self.q3 = discord.ui.TextInput(
-            label='Do you have a working mic to speak on stage?',
-            placeholder=
-            'Note that event management is prioritizing applicants who can speak on stages!',
-            required=True,
-            style=discord.TextStyle.long,
-            max_length=1018)
-        self.q4 = discord.ui.TextInput(
-            label='Why do you want to join the Events Team?',
-            style=discord.TextStyle.long,
-            max_length=1018,
-            required=True)
-        self.q5 = discord.ui.TextInput(
-            label='What does this role mean to you?',
-            required=True,
-            style=discord.TextStyle.long,
-            max_length=1018)
+        with open('data/questions.json', 'r') as f:
+            questions = json.load(f)['questions']
 
-        self.add_item(self.q1)
-        self.add_item(self.q2)
-        self.add_item(self.q3)
-        self.add_item(self.q4)
-        self.add_item(self.q5)
+        super().__init__(title='Fill this to apply for events team', timeout=3600)
+        self.bot = bot
+
+        self.questions = []
+        for q in questions:
+            text_input = discord.ui.TextInput(
+                label=q['label'],
+                placeholder=q.get('placeholder', ''),
+                style=discord.TextStyle.long if q['style'] == 'long' else discord.TextStyle.short,
+                max_length=q.get('max_length', None),
+                required=q.get('required', False),                 )
+            self.add_item(text_input)
+            self.questions.append(text_input)
 
     async def on_submit(self, interaction: discord.Interaction):
         with open('data/setup-data.json', 'r') as f:
-            config = json.load(f)
+            config_data = json.load(f)
 
-        forum_channel_id = int(config['app_submission_channel_id'])
-        cooldown_role_id = int(config['app_cooldown_role_id'])
+        forum_channel_id = int(config_data['app_submission_channel_id'])
+        cooldown_role_id = int(config_data['app_cooldown_role_id'])
 
         guild = interaction.guild
         if guild is None:
             print("Guild not found.")
             return
-
-        # print(f"Guild roles: {[role.name for role in guild.roles]}")
 
         role = get(guild.roles, id=cooldown_role_id)
         if role is None:
@@ -72,28 +47,15 @@ class EventAppModal(discord.ui.Modal,
         embed = discord.Embed(title="Event Team Application", color=0xffffff)
         embed.add_field(
             name="Submitted by",
-            value=
-            f"{interaction.user.mention} (@{interaction.user}, `{interaction.user.id}`)",
+            value=f"{interaction.user.mention} (@{interaction.user}, `{interaction.user.id}`)",
             inline=False)
-        embed.add_field(name="1. Tell us about you!",
-                        value=f"```{self.q1.value}```",
-                        inline=False)
-        embed.add_field(name="2. What is your timezone?",
-                        value=f"```{self.q2.value}```",
-                        inline=False)
-        embed.add_field(
-            name=
-            "3. Do you have a working mic and are you able to speak on stage?",
-            value=f"```{self.q3.value}```",
-            inline=False)
-        embed.add_field(
-            name="4. Why do you want to become a part of the Events Team?",
-            value=f"```{self.q4.value}```",
-            inline=False)
-        embed.add_field(
-            name="5. What does the role of an Events Team member mean to you?",
-            value=f"```{self.q5.value}```",
-            inline=False)
+
+        for i, question in enumerate(self.questions, 1):
+            embed.add_field(
+                name=f"{i}. {question.label}",
+                value=f"```{question.value}```",
+                inline=False
+            )
 
         forum_channel = guild.get_channel(forum_channel_id)
         if forum_channel is None:
@@ -111,37 +73,33 @@ class EventAppModal(discord.ui.Modal,
             print("Forum channel is not a ForumChannel.")
 
 class EventAppButton(discord.ui.View):
-
     def __init__(self, bot):
         super().__init__(timeout=None)
         self.bot = bot
         self.setup_data_file = 'data/setup-data.json'
 
     async def check_app_channel(self):
-
         if not os.path.exists(self.setup_data_file):
-          return False
-    
+            return False
+
         with open(self.setup_data_file, 'r') as file:
-          setup_data = json.load(file)
+            setup_data = json.load(file)
 
         app_submission_channel_id = setup_data.get('app_submission_channel_id')
-        
+
         return app_submission_channel_id is not None and app_submission_channel_id != ""        
 
     @discord.ui.button(label="Fill Application",
                        style=discord.ButtonStyle.blurple,
                        custom_id="fill_app",
                        emoji=config.FILL_APP)
-    async def create_tag_callback(self, interaction: discord.Interaction,
-                                  button: discord.ui.Button):
-        with open('data/setup-data.json', 'r') as f:
-            config = json.load(f)
+    async def create_tag_callback(self, interaction: discord.Interaction, button: discord.ui.Button):
+        with open(self.setup_data_file, 'r') as f:
+            config_data = json.load(f)
 
-        cooldown_role_id = int(config['app_cooldown_role_id'])
+        cooldown_role_id = int(config_data['app_cooldown_role_id'])
 
         guild = interaction.guild
-
         if guild is None:
             print("Guild not found.")
             return
@@ -154,12 +112,10 @@ class EventAppButton(discord.ui.View):
         if not await self.check_app_channel():
             await interaction.response.send_message("Event Application submission has been temporarily paused.", ephemeral=True)
             return
-            
 
         if cooldown_role in interaction.user.roles:
             embed = discord.Embed(description="⏱️  You are on cooldown.",
                                   color=0xffffff)
-            await interaction.response.send_message(embed=embed,
-                                                    ephemeral=True)
+            await interaction.response.send_message(embed=embed, ephemeral=True)
         else:
             await interaction.response.send_modal(EventAppModal(self.bot))
